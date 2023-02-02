499bf704a52f8e6f06bdaeaa2b95840a3914420f
Zachary Youngdale <zyoungdale@gmail.com>
2023-02-02 13:40:41 -0500

    initial react and tailwind setup

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <div className="font-sans flex flex-col justify-center items-center">
+      <h1 className="font-bold text-7xl m-6">Zachary Youngdale</h1>
+      <h2 className="font-semibold text-3xl">Software Engineer and Musician</h2>
+      <p className="font-medium text-2xl p-6 w-2/4 text-center">
+        Hi my name is Zach! As a former musician and psychology student, I bring
+        a unique blend of creativity and analytical thinking to my work as a
+        software engineer. With a strong background in the arts and a deep
+        understanding of human behavior, I am able to approach problem-solving
+        from multiple angles and create intuitive and user-friendly solutions.
+        Whether I'm developing new applications or optimizing existing ones, I
+        am driven by a passion for creating functional and aesthetically
+        pleasing software. I am excited to bring my diverse skill set and fresh
+        perspective to any team, and I am always looking for new challenges and
+        opportunities to grow as a developer.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
