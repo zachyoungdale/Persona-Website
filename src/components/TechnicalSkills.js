34e1f8f44f3cc6a725f7d1906a2f861374ac83fe
Zachary Youngdale <zyoungdale@gmail.com>
2023-03-30 20:41:17 -0400

    rearranged technical skills to list front end to back end

--- src/components/TechnicalSkills.js
+++ src/components/TechnicalSkills.js
@@ -19,10 +19,10 @@ function TechnicalSkills() {
       </div>
       <div className="grid lg:grid-cols-9 justify-items-center text-6xl sm:text-7xl mt-10 sm:mt-16 md:grid-cols-4 md:gap-y-8 grid-cols-2 gap-y-10">
         <FaReact className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
-        <SiRubyonrails className="hover:-translate-y-1 hover:scale-125 transition ease-in-out text-7xl sm:text-8xl" />
         <SiJavascript className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
         <SiTypescript className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
         <SiRuby className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
+        <SiRubyonrails className="hover:-translate-y-1 hover:scale-125 transition ease-in-out text-7xl sm:text-8xl" />
         <SiHtml5 className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
         <SiCss3 className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
         <SiTailwindcss className="hover:-translate-y-1 hover:scale-125 transition ease-in-out" />
